7a6c4b410aa616f2e1a285d661847889f452380f
Bradley E. Gillard <bradleygillard@gmail.com>
2011-12-11 12:32:37 -0600

    Security model with access control.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
 	before_filter :authenticate, :only => [:edit, :update]
+	before_filter :correct_user, :only => [:edit, :update]
 
   def show
     @user = User.find_by_id(params[:id])
@@ -44,6 +45,11 @@ class UsersController < ApplicationController
   
   	def authenticate
 		deny_access unless logged_in?
+	end
+	
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_path) unless current_user?(@user)
 	end
 	
 end
